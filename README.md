d64fe414503ac3655f8c87b32096ab71e62d2d9b
unknown <juliotelcel@hotmail.com>
2022-01-08 02:51:15 -0300

    creacion de lista de tareas de la aplicacion y archivos del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# MedianaVentaDiaria
+## MedianaVentaDiaria
+
+- paso1: crear los archivos del proyecto
+- Paso2: definir las ventas con los dias y el monto por dia
+- Paso3: buscar y tener los helpers necesarios
+- Paso4: crear una lista montoDia que contenga solo los montos por dia
+- Paso5: ordenar la lista montoDia
+- Paso6: crear funcion esPar En caso de que los elemento del listado sean pares y poder calcular el promedio de los 2 medios
+- Paso7: crear funcion para calcular la mediana del monto de las ventas
+- Paso8: sacar los 20% mejores montos
+- Paso9: sacar los 20% peores montos
---
+++ ventas.js
@@ -0,0 +1,123 @@
+//paso 2
+const ventas = [];
+
+ventas.push({
+  dia: "1",
+  monto: 168380,
+});
+ventas.push({
+  dia: "2",
+  monto: 351250,
+});
+ventas.push({
+  dia: "3",
+  monto: 306070,
+});
+ventas.push({
+  dia: "4",
+  monto: 139830,
+});
+ventas.push({
+  dia: "5",
+  monto: 160100,
+});
+ventas.push({
+  dia: "6",
+  monto: 319350,
+});
+ventas.push({
+  dia: "7",
+  monto: 353630,
+});
+ventas.push({
+  dia: "8",
+  monto: 229220,
+});
+ventas.push({
+  dia: "9",
+  monto: 255390,
+});
+ventas.push({
+  dia: "10",
+  monto: 326590,
+});
+ventas.push({
+  dia: "11",
+  monto: 199660,
+});
+ventas.push({
+  dia: "12",
+  monto: 146930,
+});
+ventas.push({
+  dia: "13",
+  monto: 199450,
+});
+ventas.push({
+  dia: "14",
+  monto: 187960,
+});
+ventas.push({
+  dia: "15",
+  monto: 201360,
+});
+ventas.push({
+  dia: "16",
+  monto: 252720,
+});
+ventas.push({
+  dia: "17",
+  monto: 167940,
+});
+ventas.push({
+  dia: "18",
+  monto: 133830,
+});
+ventas.push({
+  dia: "19",
+  monto: 205600,
+});
+ventas.push({
+  dia: "20",
+  monto: 147400,
+});
+ventas.push({
+  dia: "21",
+  monto: 180000,
+});
+ventas.push({
+  dia: "22",
+  monto: 236850,
+});
+ventas.push({
+  dia: "23",
+  monto: 288570,
+});
+ventas.push({
+  dia: "24",
+  monto: 276020,
+});
+ventas.push({
+  dia: "25",
+  monto: 210090,
+});
+ventas.push({
+  dia: "26",
+  monto: 175760,
+});
+ventas.push({
+  dia: "27",
+  monto: 229500,
+});
+ventas.push({
+  dia: "28",
+  monto: 190920,
+});
+ventas.push({
+  dia: "29",
+  monto: 244370,
+});
+ventas.push({
+  dia: "30",
+  monto: 187960,
+});
